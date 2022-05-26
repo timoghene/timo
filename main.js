d37557c12579600386ba04321ebfae95d058251f
timoghene <106329998+timoghene@users.noreply.github.com>
2022-05-26 15:50:13 +0100

    Add initial timo test website files

---
+++ main.js
@@ -0,0 +1,6 @@
+
+document.getElementById("create").onclick = function(){
+        alert("are you sure?");
+        document.getElementById("create").style.display ="none";
+        alert("account created successfully!!");
+    }
